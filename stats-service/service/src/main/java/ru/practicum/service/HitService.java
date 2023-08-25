5ca1441d792f6fc5c3b6d3bfd83c36f5b74a4650
Evgeniy Mishin <dubreak@yandex.ru>
2023-08-25 11:35:43 +0300

    fix tests

--- stats-service/service/src/main/java/ru/practicum/service/HitService.java
+++ stats-service/service/src/main/java/ru/practicum/service/HitService.java
@@ -8,5 +8,6 @@ import java.util.List;
 
 public interface HitService {
     HitDto saveHit(HitDto hit);
+
     List<StatsDto> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, Boolean unique);
 }
