97b2d5cf8c5ae366ee91a6fbeb767c16866ca01d
Evgeniy Mishin <dubreak@yandex.ru>
2023-08-27 00:12:21 +0300

    fix tests

--- main-service/src/main/java/ru/practicum/main/event/repository/EventRepository.java
+++ main-service/src/main/java/ru/practicum/main/event/repository/EventRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import ru.practicum.main.event.dto.EventRequestByParams;
 import ru.practicum.main.event.model.Event;
 import ru.practicum.main.user.model.User;
 
